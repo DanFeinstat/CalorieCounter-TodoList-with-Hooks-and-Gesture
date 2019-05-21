9bfa7c3790b23a327b049462a61e1537463ba20f
DanFein <danfeinstat@gmail.com>
2019-05-21 14:59:12 -0700

    Updates

---
+++ precache-manifest.a8356ed0cf05d980364e0a95346552d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "832b6dd5f478930003cc7476e445221d",
+    "url": "/CalorieCounter-TodoList-with-Hooks-and-Gesture/index.html"
+  },
+  {
+    "revision": "faa16327fe704d00e396",
+    "url": "/CalorieCounter-TodoList-with-Hooks-and-Gesture/static/css/main.82f3060f.chunk.css"
+  },
+  {
+    "revision": "895bb4a1668be42bded4",
+    "url": "/CalorieCounter-TodoList-with-Hooks-and-Gesture/static/js/2.9c52275f.chunk.js"
+  },
+  {
+    "revision": "faa16327fe704d00e396",
+    "url": "/CalorieCounter-TodoList-with-Hooks-and-Gesture/static/js/main.8fcb9bcb.chunk.js"
+  },
+  {
+    "revision": "6cf7e252f3e56d096754",
+    "url": "/CalorieCounter-TodoList-with-Hooks-and-Gesture/static/js/runtime~main.359b68c0.js"
+  },
+  {
+    "revision": "3b265d437ec1c3b3bce81affda06605c",
+    "url": "/CalorieCounter-TodoList-with-Hooks-and-Gesture/static/media/healthFood.3b265d43.jpg"
+  },
+  {
+    "revision": "d7eeae4c38ac3aa8de105883c0501b66",
+    "url": "/CalorieCounter-TodoList-with-Hooks-and-Gesture/static/media/stickyNotes.d7eeae4c.jpg"
+  }
+]);
